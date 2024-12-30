3f6d3d06d728c3611620c24192b6112da03189a4
duvoodi <amda88084@gmail.com>
2024-12-30 19:15:00 +0000

    Deploying to gh-pages from @ duvoodi/duvoodi.github.io@5194e0cafc55ca7f73813c9eecc25ead13807ea3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -150,9 +150,9 @@ ninja.data = [{
           description: "Perform basic addition and subtraction of two numbers.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-finding-the-largest-of-three-numbers",
+          title: 'Finding the Largest of Three Numbers',
+          description: "Identify the largest among three user-provided numbers.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
