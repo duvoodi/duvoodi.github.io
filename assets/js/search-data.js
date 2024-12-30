d4c59c69d26c4ed77f51f669a8a273e9e91050d2
duvoodi <amda88084@gmail.com>
2024-12-30 19:16:04 +0000

    Deploying to gh-pages from @ duvoodi/duvoodi.github.io@2b3a15df8e7849c761c6a0bb28ed434a2363232a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -155,9 +155,9 @@ ninja.data = [{
           description: "Identify the largest among three user-provided numbers.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-checking-if-a-number-is-even-or-odd",
+          title: 'Checking if a Number is Even or Odd',
+          description: "Determine whether a number is even or odd.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
